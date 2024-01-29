478853dcab6798c98f44e38cda88461c6b13290e
FrancescoBorzi <borzifrancesco@gmail.com>
2024-01-29 16:40:18 +0100

    chore: swap

--- libs/counter/src/counter.component.spec.ts
+++ libs/counter/src/counter.component.spec.ts
@@ -2,6 +2,62 @@ import { TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
 import { By } from '@angular/platform-browser';
 
+describe('CounterComponent - Component class testing', () => {
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CounterComponent],
+    }).compileComponents();
+  });
+
+  function setup() {
+    const fixture = TestBed.createComponent(CounterComponent);
+    const component = fixture.componentInstance;
+    return { fixture, component };
+  }
+
+  it('should have the counter set to 0 by default', () => {
+    const { component } = setup();
+
+    expect(component.count).toBe(0);
+  });
+
+  it('should increase the counter when clicking on the increase button', () => {
+    const { component } = setup();
+
+    component.increase();
+
+    expect(component.count).toBe(1);
+  });
+
+  it('should decrease the counter if the current value is greater than 0 when clicking on the decrease button', () => {
+    const { component } = setup();
+    component.count = 2;
+
+    component.decrease();
+
+    expect(component.count).toBe(1);
+  });
+
+  it('should NOT decrease the counter if the current value is 0 when clicking on the decrease button', () => {
+    const { component } = setup();
+    component.count = 0;
+
+    component.decrease();
+
+    expect(component.count).toBe(0);
+  });
+
+  it('should reset the counter when clicking the reset button', () => {
+    const { component } = setup();
+    component.count = 123;
+
+    component.reset();
+
+    expect(component.count).toBe(0);
+  });
+});
+
 describe('CounterComponent - Component DOM testing', () => {
 
   beforeEach(async () => {
@@ -63,61 +119,5 @@ describe('CounterComponent - Component DOM testing', () => {
     fixture.detectChanges();
 
     expect(fixture.debugElement.query(By.css(`[data-test-id="count"]`)).nativeElement.innerHTML).toEqual('0');
-  });
-});
-
-describe('CounterComponent - Component class testing', () => {
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CounterComponent],
-    }).compileComponents();
-  });
-
-  function setup() {
-    const fixture = TestBed.createComponent(CounterComponent);
-    const component = fixture.componentInstance;
-    return { fixture, component };
-  }
-
-  it('should have the counter set to 0 by default', () => {
-    const { component } = setup();
-
-    expect(component.count).toBe(0);
-  });
-
-  it('should increase the counter when clicking on the increase button', () => {
-    const { component } = setup();
-
-    component.increase();
-
-    expect(component.count).toBe(1);
-  });
-
-  it('should decrease the counter if the current value is greater than 0 when clicking on the decrease button', () => {
-    const { component } = setup();
-    component.count = 2;
-
-    component.decrease();
-
-    expect(component.count).toBe(1);
-  });
-
-  it('should NOT decrease the counter if the current value is 0 when clicking on the decrease button', () => {
-    const { component } = setup();
-    component.count = 0;
-
-    component.decrease();
-
-    expect(component.count).toBe(0);
-  });
-
-  it('should reset the counter when clicking the reset button', () => {
-    const { component } = setup();
-    component.count = 123;
-
-    component.reset();
-
-    expect(component.count).toBe(0);
   });
 });
